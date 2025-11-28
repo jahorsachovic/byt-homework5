64446efdc40a4a31a5781d25cc093b932b854eb1
jahorsachovic <s26864@pjwstk.edu.pl>
2025-11-28 15:36:08 +0100

    Implement database factory

--- DesignPattern/Factory/DatabaseFactory.cs
+++ DesignPattern/Factory/DatabaseFactory.cs
@@ -1,10 +1,24 @@
-﻿namespace DesignPattern.Factory
+﻿using DesignPattern.Exceptions;
+
+namespace DesignPattern.Factory
 {
     public static class DatabaseFactory
     {
-        //public static IDatabase CreateDatabase(...)
-        //{
-        //  ...
-        //}
+        public static IDatabase CreateDatabase(DatabaseType dbType)
+        {
+            switch (dbType)
+            {
+                case DatabaseType.Oracle:
+                    return new OracleDatabase();
+                
+                case DatabaseType.SqlServer:
+                    return new SqlServerDatabase();
+                
+                default:
+                    throw new IllegalDatabaseType($"Database {dbType} is not supported");
+            }
+            
+            
+        }
     }
 }
